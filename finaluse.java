81e488d274b60ae7cca67812c500a159c9516210
Nalli-vyshu <123969722+Nalli-vyshu@users.noreply.github.com>
2023-03-02 12:40:38 +0530

    javapractise2-3-23

---
+++ finaluse.java
@@ -0,0 +1,9 @@
+class finaluse
+{
+  public static void main(String args[])
+  {
+    final int num=15;
+    System.out.println(num);
+  }
+
+}
---
+++ fog.java
@@ -0,0 +1,12 @@
+class fog
+{
+  public static void main(String args[])
+  {
+  
+  }
+   Static 
+    {
+     System.out.println("Hello World");
+    }
+ }
+ 
